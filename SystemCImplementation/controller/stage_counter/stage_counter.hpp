7c82594008b77c5b535982fda12fd92a2a2bb5a3
Hugo Zuniga Calvo <hzc9107@gmail.com>
2017-11-13 23:23:08 +0100

    new: Add stage counter implementation

---
+++ SystemCImplementation/controller/stage_counter/stage_counter.hpp
@@ -0,0 +1,37 @@
+#include<systemc>
+#ifndef STAGE_CNT_H
+#define STAGE_CNT_H
+template<unsigned int stage_width>
+SC_MODULE(stage_counter){
+  // Inputs
+  sc_core::sc_in_clk clk;
+  sc_core::sc_in<bool> reset,
+                       enable,
+                       stage_finish;
+
+
+  // Outputs
+  sc_core::sc_out<sc_dt::sc_uint<stage_width> > stage_cnt;
+
+  // Internal Var
+  sc_dt::sc_uint<stage_width> stage_cnt;
+  // Process
+  void count_stage();
+
+  SC_CTOR(stage_counter){
+    SC_METHOD(count_stage);
+      sensitive << clk.pos();
+  }
+};
+
+template<unsigned int stage_width>
+void stage_counter<stage_width>::count_stage(){
+  if(reset){
+    stage_cnt = 0;
+  } else {
+    if(enable && stage_finish){
+      ++stage_cnt;
+    }
+  }
+}
+#endif
