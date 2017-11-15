2ccdaee4a1d17eb9d30f8ebf477b1def1a92b42d
Hugo Zuniga Calvo <hzc9107@gmail.com>
2017-11-15 01:39:28 +0100

    chg: Templerize controller implementation

--- SystemCImplementation/controller/controller.hpp
+++ SystemCImplementation/controller/controller.hpp
@@ -9,11 +9,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SAMPLE_WIDTH 11
-#define ADDRESS_WIDTH 10
-#define STAGE_WIDTH 4
-#define NUMBER_OF_SUPPORTED_SAMPLE_SIZE 5
-
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
 SC_MODULE(controller){
   // Inputs
   sc_core::sc_in_clk clk;
@@ -22,10 +18,10 @@ SC_MODULE(controller){
                        start,
                        pipe_finish;
 
-  sc_core::sc_in<sc_dt::sc_uint<SAMPLE_WIDTH> > number_of_samples;
+  sc_core::sc_in<sc_dt::sc_uint<sample_width> > number_of_samples;
 
   // Outputs
-  sc_core::sc_out<sc_dt::sc_uint<ADDRESS_WIDTH> > address_low,
+  sc_core::sc_out<sc_dt::sc_uint<address_width> > address_low,
                                                   address_high,
                                                   twiddle_address;
 
@@ -36,15 +32,15 @@ SC_MODULE(controller){
                         stage_finish;
 
   // Instances
-  address_generator<ADDRESS_WIDTH, ADDRESS_WIDTH-1, STAGE_WIDTH> addr_gen;
+  address_generator<address_width, address_width-1, stage_width> addr_gen;
 
   enable_generator en_gen;
 
-  stage_counter<STAGE_WIDTH> stage_cnt;
+  stage_counter<stage_width> stage_cnt;
 
-  twiddle_generator<ADDRESS_WIDTH, STAGE_WIDTH> twid_gen;
+  twiddle_generator<address_width, stage_width> twid_gen;
 
-  max_generator<NUMBER_OF_SUPPORTED_SAMPLE_SIZE, ADDRESS_WIDTH-1, STAGE_WIDTH> max_gen;
+  max_generator<number_of_supported_sample_size, address_width-1, stage_width> max_gen;
 
   // Internal signals
     sc_core::sc_signal<bool> internal_reset,
@@ -54,17 +50,12 @@ SC_MODULE(controller){
                              pipe_en_internal,
                              mem_selector;
 
-    sc_core::sc_signal<sc_dt::sc_uint<ADDRESS_WIDTH - 1> > max_count;
+    sc_core::sc_signal<sc_dt::sc_uint<address_width - 1> > max_count;
 
-    sc_core::sc_signal<sc_dt::sc_uint<STAGE_WIDTH> > stage_count,
+    sc_core::sc_signal<sc_dt::sc_uint<stage_width> > stage_count,
                                                      max_stage;
 
-    sc_core::sc_signal<sc_dt::sc_uint<NUMBER_OF_SUPPORTED_SAMPLE_SIZE> > sample_width_selector;
-
-    // TODO: create process for pipe_en_internal to split into mem_en and pipe_en
-    // TODO: Create process to continuously assign stage_finish_internal to stage_finish
-    // TODO: Create proces for generating internal_reset
-    // TODO: Create process for assigning 5 MSbits to the sample_width_selector
+    sc_core::sc_signal<sc_dt::sc_uint<number_of_supported_sample_size> > sample_width_selector;
 
     // Processes
     void cont_assign_pipe_en_internal();
@@ -116,12 +107,6 @@ SC_MODULE(controller){
     max_gen.max_count(max_count);
     max_gen.max_stage(max_stage);
 
-
-    // TODO: create process for pipe_en_internal to split into mem_en and pipe_en
-    // TODO: Create process to continuously assign stage_finish_internal to stage_finish
-    // TODO: Create proces for generating internal_reset
-    // TODO: Create process for assigning 5 MSbits to the sample_width_selector
-
     // Processes
     SC_METHOD(cont_assign_pipe_en_internal);
       sensitive << pipe_en_internal;
@@ -136,24 +121,29 @@ SC_MODULE(controller){
   }
 };
 
-void controller::cont_assign_pipe_en_internal(){
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
+void controller<sample_width, address_width, stage_width, number_of_supported_sample_size>::cont_assign_pipe_en_internal(){
   mem_en.write(pipe_en_internal.read());
   pipe_en.write(pipe_en_internal.read());
 }
 
-void controller::cont_assign_stage_finish(){
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
+void controller<sample_width, address_width, stage_width, number_of_supported_sample_size>::cont_assign_stage_finish(){
   stage_finish.write(stage_finish_internal.read());
 }
 
-void controller::create_internal_reset(){
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
+void controller<sample_width, address_width, stage_width, number_of_supported_sample_size>::create_internal_reset(){
   internal_reset = reset.read() || ((max_stage.read() == stage_count.read()) && pipe_finish.read());
 }
 
-void controller::cont_assign_sample_width_selector(){
-  sample_width_selector.write(number_of_samples.read().range(SAMPLE_WIDTH-1, SAMPLE_WIDTH-5));
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
+void controller<sample_width, address_width, stage_width, number_of_supported_sample_size>::cont_assign_sample_width_selector(){
+  sample_width_selector.write(number_of_samples.read().range(sample_width-1, sample_width-5));
 }
 
-void controller::cont_assign_mem_selector(){
+template<unsigned int sample_width, unsigned int address_width, unsigned int stage_width, unsigned int number_of_supported_sample_size>
+void controller<sample_width, address_width, stage_width, number_of_supported_sample_size>::cont_assign_mem_selector(){
   if(stage_count.read()[0]){
     mem_selector.write(true);
   } else {
